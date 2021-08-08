973c56fe999c564984f6b6cdfb0de7d4940df1c0
Juan Soto <sotoj88515@gmail.com>
2021-08-07 18:46:29 -0700

    build first page with template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import { even } from 'check-types';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Form from './form';
+import Site from './site';
+import './style.css';
 
 const App = () => {
-  return <div>
-    Hello World
-  </div>
-}
+    const [user, setUser] = useState({});
+    const [formDone, setFormDone] = useState(false);
+      return <div className="app">
+      { formDone ? <Site user={user} setUser={setUser} setFormDone={setFormDone}/> : <Form user={user} setUser={setUser} setFormDone={setFormDone}/> }
+      </div>
+    }
 
 ReactDOM.render(
   <App />,
